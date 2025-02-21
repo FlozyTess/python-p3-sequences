362521192961d9029c594018a341f5fcab65e0e0
FlozyTess <cecilrency23@gmail.com>
2025-02-21 08:13:41 +0300

    added sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    """Prints a list of Fibonacci numbers up to the given length."""
+    if length <= 0:
+        print([])
+        return
+    
+    fibonacci = [0, 1] if length > 1 else [0]  
+
+    while len(fibonacci) < length:
+        fibonacci.append(fibonacci[-1] + fibonacci[-2])
+    
+    print(fibonacci)
     pass
